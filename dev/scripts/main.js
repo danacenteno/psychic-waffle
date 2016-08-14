8a595361853bd188ba8bc17f2e83ffcbbc762a33
Dana Centeno <danacentenodes@gmail.com>
2016-08-13 23:14:37 -0400

    SWAPI info and image links showing up... randomizing now

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -45,26 +45,14 @@ swApp.getData = function(){
 	.then(function(filmData,picData){
 		// console.log(filmData,picData);
 		var movies = filmData[0].results;
-		console.log(movies);
+		// console.log(movies);
 
 		var images = picData[0];
-		console.log(images);
  
 		// need to figure out how to dynamically generate titles and images without making a billion variables!
 
-		// var title = movies[1].title;	
-		// // console.log(title);
-
-		// var description = movies[1].opening_crawl;
-		// console.log(description);
-
-		// var hope = images[0];
-		// // console.log(hope);
-		// var empire = images[1];
-		// // console.log(empire);
-		// var jedi = images[2];
-		// // console.log(jedi);
-		// var force = images[3];
+		swApp.displayData(movies,images);
+		// console.log(movies,images);
 	});
 	// swApp.getData(movies.title);
 };
@@ -75,8 +63,58 @@ swApp.getData = function(){
 
 // there will also be an option to grab a color palette using colorthief JS.
 
-swApp.displayData = function(){
+swApp.displayData = function(filminfo,pix){
+	// console.log(filminfo,pix);
 
+	filminfo.forEach(function(films){
+		var filmsTitle = films.title;
+		$('.title').text(filmsTitle);
+		// console.log(filmsTitle);
+
+		var filmDescription = films.opening_crawl;
+		$('.description').text(filmDescription);
+		// console.log(filmDescription);
+	});
+
+	var hope = pix[0];
+	// console.log(hope);
+
+	var hopeRandom = (Math.floor(Math.random() * hope.length));
+	console.log(hope[hopeRandom]);
+
+	// var hopeImg = $('<img>').attr('src', hopeRandom);
+	// console.log(hopeImg);
+
+	// var hopeDiv = $('<div>')
+	// 				.addClass('results').append(hopeImg);
+
+	// $('#images').append(hopeDiv);
+
+	// var empire = pix[1];
+	// console.log(empire);
+
+	// var jedi = pix[2];
+	// console.log(jedi);
+
+	// var force = pix[3];
+	// console.log(force);
+
+
+	// var title = filminfo[0].title;	
+	// $('.title').text(title);
+
+	// var description = filminfo[0].opening_crawl;
+	// $('.description').text(description);
+
+	// var hope = pix[0].img1;
+	// console.log(hope);
+
+	// var movieimg = $('<img>').attr('src', hope);
+	// var empire = images[1];
+	// // console.log(empire);
+	// var jedi = images[2];
+	// // console.log(jedi);
+	// var force = images[3];
 };
 
 //document ready!
